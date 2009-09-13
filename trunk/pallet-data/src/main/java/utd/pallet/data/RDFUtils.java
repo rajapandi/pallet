c1c7f0d752d1d1ce0e3d9278c4c8af785fa9cc81
pralabhkumar@gmail.com <pralabhkumar@gmail.com@775479ba-5cdc-11de-84d6-ef71daef37f8>
2009-09-13 01:08:50 +0000

    RDFutils is created which gives the classification model.

--- trunk/pallet-data/src/main/java/utd/pallet/data/RDFUtils.java
+++ trunk/pallet-data/src/main/java/utd/pallet/data/RDFUtils.java
@@ -1,20 +1,61 @@
 package utd.pallet.data;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import cc.mallet.classify.Classification;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 public class RDFUtils {
  
 	public static Model createModelWithClassifications (ArrayList<MalletAccuracyVector>  accVector,
-											ArrayList<Classification> classificationList) {
 		
+			ArrayList<Classification> classificationList) {
 		@SuppressWarnings("unused")
-		Model rdfModel = null;
+		Model rdfModel = ModelFactory.createDefaultModel();
+		try{
 		
-		return null;		
+		String uriAddress="http://marathonminds.com//MalletClassification//";
+		
+		int i=0;
+		for (MalletAccuracyVector av : accVector) {
+			String name = (String)av.getName();
+		    HashMap <String,Double> labelVector = av.getAccuracyVector ();
+		    Iterator<String> iterator = labelVector.keySet().iterator();
+		    String bestLabel = av.getBestLabel ();
+		    
+		    Resource originalResource=rdfModel.createResource(name);
+			Resource bestLabelResource=rdfModel.createResource(uriAddress+bestLabel+i);
+			Property bestLabelProperty=rdfModel.createProperty(uriAddress+"#hasBestLabel");
+			originalResource.addProperty(bestLabelProperty, bestLabelResource);
+			while(iterator.hasNext())
+			{
+				String Label=iterator.next();
+				Double confidencValue=labelVector.get(Label);
+				Property malletConfidenceValue=rdfModel.createProperty(uriAddress+"#MalletConfidenceValue");
+				Resource labelResource=rdfModel.createResource(uriAddress+Label+i);
+				Property associatedWith=rdfModel.createProperty(uriAddress+"#associatedWith");
+				Property hasValue=rdfModel.createProperty(uriAddress+"#hasValue");
+				labelResource.addProperty(malletConfidenceValue, confidencValue.toString());
+			    labelResource.addProperty(associatedWith, originalResource);
+			    labelResource.addProperty(hasValue, Label);
+			}
+			i++;
+		    
+			
+		}
+		return rdfModel;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		
+		return rdfModel;		
 	}
 	
 } 
