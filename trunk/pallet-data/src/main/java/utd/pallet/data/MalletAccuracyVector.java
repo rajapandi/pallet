193a83c3189a7b6410c2ed6713f1f35b1b05fc1b
pralabhkumar@gmail.com <pralabhkumar@gmail.com@775479ba-5cdc-11de-84d6-ef71daef37f8>
2009-10-20 04:30:50 +0000

    Comments are added

--- trunk/pallet-data/src/main/java/utd/pallet/data/MalletAccuracyVector.java
+++ trunk/pallet-data/src/main/java/utd/pallet/data/MalletAccuracyVector.java
@@ -4,113 +4,142 @@ import java.util.HashMap;
 
 import cc.mallet.classify.Classification;
 
-
 /**
- * 
+ * This class contains the methods which are used to get the source ,name and
+ * other Important information about Mallet Instances. This class also tells
+ * about the accuracy vector and best Label after the classification.
  * 
  */
 public class MalletAccuracyVector {
-	
-	Object source = null;
-	Object name = null;
-	String bestLabel = null;
-	HashMap<String, Double> labelAccuracyVector = new HashMap<String, Double> ();
-	Classification cl = null;
-	
-	/**
-	 * 
-	 */
-	public MalletAccuracyVector () {};
-	
-	/**
-	 * Initializes the classification for which the accuracy vector as to be calculated.
-	 * @param classification
-	 * @throws java.lang.NullPointerException
-	 * @throws java.lang.Exception
-	 */
-	public void setAccuracy (Classification classification) 
-					throws java.lang.NullPointerException, java.lang.Exception {
-		if (classification == null)
-			throw new NullPointerException ("Invalid Classification object");
-	
-		cl = classification;
-		source = classification.getInstance().getSource();
-		name = classification.getInstance().getName();
-		bestLabel = classification.getLabeling().getBestLabel().toString();
-		
-		// FeatureVector fv = (FeatureVector) classification.getInstance().getData();
-		//int []indices = new int [fv.getIndices().length];
-		//Alphabet dict = fv.getAlphabet();
-		
-		for (int i = 0; i < classification.getLabelVector().getAlphabet().size(); i++) {
-			labelAccuracyVector.put (classification.getLabelVector().getAlphabet().lookupObject(i).toString(),
-									classification.getLabelVector().value(i));
-			//labelAccuracyVector.put(dict.lookupObject(indices[i]).toString(), value);
-			
-			/*System.out.println ("Label - " + 
-						classification.getLabelVector().getAlphabet().lookupObject(i).toString() + 
-						",Value  - " + classification.getLabelVector().value(i));*/
-		}
-		//System.out.println ("---------------------------");
-	}
-	
 
-	/**
+    /**
+     * It represents the source of the Mallet Instances
+     */
+    Object source = null;
+    /**
+     * It represents the name of the Mallet Instances
+     */
+    Object name = null;
+    /**
+     * It represent the best Label associated with particular Mallet Instances
+     * after classification.
+     */
+    String bestLabel = null;
+    /**
+     * It represents the label Accuracy Vector.
+     */
+    HashMap<String, Double> labelAccuracyVector = new HashMap<String, Double>();
+    /**
 	 * 
-	 * @return Hashmap
-	 * @throws java.lang.Exception
 	 */
-	public HashMap<String, Double> getAccuracyVector ()  throws java.lang.Exception {			
-		if (labelAccuracyVector.isEmpty() == true)
-			throw new Exception ("Empty Accuracy Vector");
-					
-		return labelAccuracyVector;
-	}
-	
-	/**
+    Classification cl = null;
+
+    /**
 	 * 
-	 * @return
-	 * @throws java.lang.Exception
 	 */
-	public double getAccuracy () throws java.lang.NullPointerException {
-		
-		if (cl == null)
-			throw new NullPointerException ("Classification not set");
-		int index = cl.getLabeling().getBestLabel().getBestIndex();
-		return cl.getLabeling().value(index);
-	}
-	
-	/**
-	 * 
-	 * @return
-	 * @throws java.lang.Exception
-	 */
-	public String getBestLabel () throws java.lang.NullPointerException {
-		if (cl == null)
-			throw new NullPointerException ("Classification not set");			
-		return cl.getLabeling().getBestLabel().toString();
-	}
-	
-	/**
-	 * 
-	 * @return
-	 * @throws java.lang.NullPointerException
-	 */
-	public Object getSource () throws java.lang.NullPointerException {
-		if (cl == null)
-			throw new NullPointerException ("Classification not set");
-		
-		return this.source; // can be null
-	}
-	
-	/**
-	 * 
-	 * @return
-	 * @throws java.lang.NullPointerException
-	 */
-	public Object getName () throws java.lang.NullPointerException {
-		if (cl == null)
-			throw new NullPointerException ("Classification not set");			
-		return this.name; // can be null			
-	}
+    public MalletAccuracyVector() {
+    };
+
+    /**
+     * Initializes the classification for which the accuracy vector as to be
+     * calculated.
+     * 
+     * @param classification
+     *            : It takes the classification object.
+     * @throws java.lang.NullPointerException
+     * @throws java.lang.Exception
+     */
+    public void setAccuracy(Classification classification)
+            throws java.lang.NullPointerException, java.lang.Exception {
+        if (classification == null)
+            throw new NullPointerException("Invalid Classification object");
+
+        cl = classification;
+        source = classification.getInstance().getSource();
+        name = classification.getInstance().getName();
+        bestLabel = classification.getLabeling().getBestLabel().toString();
+
+        // FeatureVector fv = (FeatureVector)
+        // classification.getInstance().getData();
+        // int []indices = new int [fv.getIndices().length];
+        // Alphabet dict = fv.getAlphabet();
+
+        for (int i = 0; i < classification.getLabelVector().getAlphabet()
+                .size(); i++) {
+            labelAccuracyVector.put(classification.getLabelVector()
+                    .getAlphabet().lookupObject(i).toString(), classification
+                    .getLabelVector().value(i));
+            // labelAccuracyVector.put(dict.lookupObject(indices[i]).toString(),
+            // value);
+
+            /*
+             * System.out.println ("Label - " +
+             * classification.getLabelVector().getAlphabet
+             * ().lookupObject(i).toString() + ",Value  - " +
+             * classification.getLabelVector().value(i));
+             */
+        }
+        // System.out.println ("---------------------------");
+    }
+
+    /**
+     * 
+     * @return Hashmap : This HashMap contains the Accuracy Vector.
+     * @throws java.lang.Exception
+     */
+    public HashMap<String, Double> getAccuracyVector()
+            throws java.lang.Exception {
+        if (labelAccuracyVector.isEmpty() == true)
+            throw new Exception("Empty Accuracy Vector");
+
+        return labelAccuracyVector;
+    }
+
+    /**
+     * 
+     * @return labeling : It returns the accuracy for particular Label.
+     * @throws java.lang.NullPointerException
+     */
+    public double getAccuracy() throws java.lang.NullPointerException {
+
+        if (cl == null)
+            throw new NullPointerException("Classification not set");
+        int index = cl.getLabeling().getBestLabel().getBestIndex();
+        return cl.getLabeling().value(index);
+    }
+
+    /**
+     * 
+     * @return String : It returns the best Label of the classification for
+     *         particular Mallet Instances.
+     * @throws java.lang.NullPointerException
+     */
+    public String getBestLabel() throws java.lang.NullPointerException {
+        if (cl == null)
+            throw new NullPointerException("Classification not set");
+        return cl.getLabeling().getBestLabel().toString();
+    }
+
+    /**
+     * 
+     * @return Object
+     * @throws java.lang.NullPointerException
+     */
+    public Object getSource() throws java.lang.NullPointerException {
+        if (cl == null)
+            throw new NullPointerException("Classification not set");
+
+        return this.source; // can be null
+    }
+
+    /**
+     * 
+     * @return Object
+     * @throws java.lang.NullPointerException
+     */
+    public Object getName() throws java.lang.NullPointerException {
+        if (cl == null)
+            throw new NullPointerException("Classification not set");
+        return this.name; // can be null
+    }
 }
