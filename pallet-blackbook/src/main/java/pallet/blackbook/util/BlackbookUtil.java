f0c1102bf3ce1d1642d0d3edd4adbcc5c002ed13
2byrds <2byrds@gmail.com>
2009-12-18 13:06:42 +0000

    good rdf for mallet classify

--- pallet-blackbook/src/main/java/pallet/blackbook/util/BlackbookUtil.java
+++ pallet-blackbook/src/main/java/pallet/blackbook/util/BlackbookUtil.java
@@ -4,9 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 import security.ejb.client.User;
+import utd.pallet.data.RDFUtils;
 import blackbook.jena.util.JenaModelFactory;
 import blackbook.metadata.api.AlgorithmMetadata;
 import blackbook.metadata.api.DataSourceMetadata;
+import blackbook.metadata.api.MetadataManagerIfc;
 import blackbook.metadata.manager.MetadataManagerFactory;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -16,27 +18,23 @@ public class BlackbookUtil {
 	public static Model persist2BlackbookAssertions(Model model,
 			String dsName, User user) throws Exception {
 
-		MetadataManagerFactory.getInstance().createAssertionsDS(dsName);
+		MetadataManagerIfc mm = MetadataManagerFactory.getInstance();
+		mm.createAssertionsDS(dsName);
 
-		DataSourceMetadata dsm = MetadataManagerFactory.getInstance()
-				.getDataSourceMetadataByName(dsName);
+		DataSourceMetadata dsm = mm.getDataSourceMetadataByName(dsName);
 
 		Set<String> namespace = new HashSet<String>();
-		namespace.add("urn:mallet:");
+		namespace.add(RDFUtils.MALLET_NAMESPACE);
 		dsm.setNamespace(namespace);
+		dsm.setMaxStatements(1000000);
+		dsm.setMaxUris(1000000);
 
-		AlgorithmMetadata alg = MetadataManagerFactory.getInstance()
-				.getAlgorithmMetadata("Jena Expand");
-		alg.setClassName("blackbook.algorithm.jena.JenaExpand");
-		alg.setName("Jena Expand");
+		AlgorithmMetadata alg = mm.getAlgorithmMetadata("Jena Expand");
 		dsm.setExpandAlgorithm(alg);
-		AlgorithmMetadata alg2 = MetadataManagerFactory.getInstance()
-				.getAlgorithmMetadata("Jena and Lucene Persist");
-		alg.setClassName("blackbook.algorithm.jena.JenaAndLucenePersist");
-		alg.setName("Jena and Lucene Persist");
+		AlgorithmMetadata alg2 = mm.getAlgorithmMetadata("Jena and Lucene Persist");
 		dsm.setPersistAlgorithm(alg2);
 
-		MetadataManagerFactory.getInstance().modifyMetadataObject(dsm);
+		mm.modifyMetadataObject(dsm);
 
 		Model newModel = JenaModelFactory.openModelByName(dsName, user);
 		newModel.add(model);
